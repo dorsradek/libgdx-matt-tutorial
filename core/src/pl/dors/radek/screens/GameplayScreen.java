477c4d867622737e6a5981a6ddb06edb7645d873
rdors <dors.radek@gmail.com>
2016-07-18 00:18:37 +0200

    added player to GameplayScreen

--- core/src/pl/dors/radek/screens/GameplayScreen.java
+++ core/src/pl/dors/radek/screens/GameplayScreen.java
@@ -1,11 +1,14 @@
 package pl.dors.radek.screens;
 
 import pl.dors.radek.MattTutorial;
+import pl.dors.radek.entities.Player;
 
 /**
  * Created by rdors on 2016-07-17.
  */
 public class GameplayScreen extends AbstractScreen {
+
+    private Player player;
 
     public GameplayScreen(MattTutorial game) {
         super(game);
@@ -13,5 +16,25 @@ public class GameplayScreen extends AbstractScreen {
     }
 
     private void init() {
+        initPlayer();
+    }
+
+    private void initPlayer() {
+        player = new Player();
+        stage.addActor(player);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update();
+
+        spriteBatch.begin();
+        stage.draw();
+        spriteBatch.end();
+    }
+
+    private void update() {
+        stage.act();
     }
 }
