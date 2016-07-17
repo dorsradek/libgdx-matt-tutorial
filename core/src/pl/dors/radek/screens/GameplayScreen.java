cfe0499683846f9d814aa1a6a56f66ad1c81cf30
rdors <dors.radek@gmail.com>
2016-07-17 23:55:25 +0200

    Dummy GameplayScreen

---
+++ core/src/pl/dors/radek/screens/GameplayScreen.java
@@ -0,0 +1,17 @@
+package pl.dors.radek.screens;
+
+import pl.dors.radek.MattTutorial;
+
+/**
+ * Created by rdors on 2016-07-17.
+ */
+public class GameplayScreen extends AbstractScreen {
+
+    public GameplayScreen(MattTutorial game) {
+        super(game);
+        init();
+    }
+
+    private void init() {
+    }
+}
