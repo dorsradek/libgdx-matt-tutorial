45fba5e84697b6984b0770fe8bf6ed48b102946e
rdors <dors.radek@gmail.com>
2016-07-18 23:02:30 +0200

    added Preferences

--- core/src/pl/dors/radek/MattTutorial.java
+++ core/src/pl/dors/radek/MattTutorial.java
@@ -1,9 +1,16 @@
 package pl.dors.radek;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import pl.dors.radek.screens.SplashScreen;
 
+import static com.badlogic.gdx.graphics.g2d.ParticleEmitter.SpawnShape.point;
+
 public class MattTutorial extends Game {
+
+    public final static String PREFS_NAME = "pl.dors.radek.matt.tutorial";
+    public final static String PREFS_SCORE = "pl.dors.radek.matt.tutorial.score";
 
     public static final int WIDTH = 480;
     public static final int HEIGHT = 700;
@@ -12,13 +19,27 @@ public class MattTutorial extends Game {
     private boolean paused;
     private int points;
 
+    private Preferences prefs;
+
     @Override
     public void create() {
+        init();
         this.setScreen(new SplashScreen(this));
+    }
+
+    private void init() {
+        prefs = Gdx.app.getPreferences(PREFS_NAME);
+        loadScore();
+    }
+
+    private void loadScore() {
+        points = prefs.getInteger(PREFS_SCORE);
     }
 
     public void addPoint() {
         points++;
+        prefs.putInteger(PREFS_SCORE, points);
+        prefs.flush();
     }
 
     /**
