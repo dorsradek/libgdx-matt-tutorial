76af4cab2a276336479c815314bd9120ae7cf8ab
Radoslaw Dors <radoslaw.dors@comarch.pl>
2016-07-19 16:06:18 +0200

    added player asset

--- core/src/pl/dors/radek/entities/Player.java
+++ core/src/pl/dors/radek/entities/Player.java
@@ -13,14 +13,14 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
  */
 public class Player extends Image {
 
-    private final static int WIDTH = 77;
-    private final static int HEIGHT = 152;
+    private final static int WIDTH = 150;
+    private final static int HEIGHT = 180;
 
     private final static int STARTING_X = 200;
     private final static int STARTING_Y = 300;
 
     public Player() {
-        super(new Texture("badlogic.jpg"));
+        super(new Texture("player.png"));
         this.setOrigin(WIDTH / 2, HEIGHT / 2);
         this.setSize(WIDTH, HEIGHT);
         this.setPosition(STARTING_X, STARTING_Y);
